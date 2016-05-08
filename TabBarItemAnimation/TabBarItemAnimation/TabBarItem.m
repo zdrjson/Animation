2dd51c6f6d0af59dadf35849b6b1c8fcdcefc533
张德荣 <zdrjson@sina.com>
2016-05-08 15:03:27 +0800

    test tabbarAnimation

---
+++ TabBarItemAnimation/TabBarItemAnimation/TabBarItem.m
@@ -0,0 +1,21 @@
+//
+//  TabBarItem.m
+//  TabBarItemAnimation
+//
+//  Created by 张德荣 on 16/5/8.
+//  Copyright © 2016年 JsonZhang. All rights reserved.
+//
+
+#import "TabBarItem.h"
+
+@implementation TabBarItem
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
